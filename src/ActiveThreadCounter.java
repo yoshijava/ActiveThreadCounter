6164125491293ea37b85b216f28af22d3c4e728d
yoshi <yoshijava@gmail.com>
2015-04-23 01:12:58 -0700

    Stupid improvement.

--- src/ActiveThreadCounter.java
+++ src/ActiveThreadCounter.java
@@ -1,30 +1,39 @@
+import java.io.*;
 
 class ActiveThreadCounter {
 
 	private long pid;
 
-	public ActiveThreadCounter() {
+	public ActiveThreadCounter(long pid) {
 		// If OS is not linux, just quit.
 		checkOS();
-
-
-
-	}
-
-	public void setMonitoredPID(long pid) {
 		this.pid = pid;
+		checkPID();
 	}
 
+
+	private void checkPID() {
+		File file = new File("/proc/" + pid + "/");
+		System.out.println("Checking whether " + pid + " exists...");
+		if (file.exists() && file.isDirectory()) {
+			System.out.println("Exist.");
+		}
+
+	}
+	
 	private void checkOS() {
 		String OS = System.getProperty("os.name");
 		if( OS.equalsIgnoreCase("linux") == false ) {
 			System.out.println("Only linux OSs are supported");
-			System.exit(0);
+			System.exit(1);
 		}
 	}
 
     public static void main(String[] args) {
-        ActiveThreadCounter obj = new ActiveThreadCounter();
+    	long pid = Long.parseLong(args[0]);
+        ActiveThreadCounter obj = new ActiveThreadCounter(pid);
+        System.out.println("End.");
+
     }
 }
 
